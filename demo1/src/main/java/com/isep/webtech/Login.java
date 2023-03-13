211cbbcffbdcb2fad86952b3a1b9b4bc46108d59
Thomas Chardo <tchardonnens@juniorisep.com>
2023-03-13 17:17:38 +0100

    OMG ca marche

---
+++ demo1/src/main/java/com/isep/webtech/Login.java
@@ -0,0 +1,27 @@
+package com.isep.webtech;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+    public void init() {
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<table>");
+        out.println("<tr><td>First Name</td><td>" + request.getParameter("first_name") + "</td></tr>");
+        out.println("<tr><td>Last Name</td><td>" + request.getParameter("last_name") + "</td></tr>");
+        out.println("<tr><td>Birth date</td><td>" + request.getParameter("birth_date") + "</td></tr>");
+        out.println("<table>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
